c2eda84a73c4180b3e22d3d158ee4d9a126f2c14
Kampaku <78536944+Kirinochka@users.noreply.github.com>
2022-01-21 20:38:27 +0300

    update

--- src/js/Validator.js
+++ src/js/Validator.js
@@ -1,6 +1,6 @@
 class Validator {
   static validateUsername(name) {
-    const regexp = /^[A-Za-z]([A-Za-z-_]?([0-9]){0,3})+[A-Za-z]$/g;
+    const regexp = /^[A-Za-z]([A-Za-z-_]|[A-Za-z-_]([0-9]){0,3})*[A-Za-z]$/g;
     return regexp.test(name);
   }
 }
--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -7,5 +7,5 @@ test('should validate', () => {
   expect(Validator.validateUsername('wqe43_-r')).toBeTruthy();
   expect(Validator.validateUsername('wqe43_-')).toBeFalsy();
   expect(Validator.validateUsername('1wqe43_-r')).toBeFalsy();
-  expect(Validator.validateUsername('1wqe4344_-r')).toBeFalsy();
+  expect(Validator.validateUsername('wqe4344_-r')).toBeFalsy();
 });
